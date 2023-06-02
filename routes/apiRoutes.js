e78b7e473c92b38533c2107574e7b852f4368e07
anju <anjualfino@gmail.com>
2023-06-02 16:18:28 +0800

    adding apiRoutes changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,21 @@
 const fs=require('fs');
 const router=require('express').Router();
-
+const express = require('express');
+const app = express();
 
 // GET Route for homepage
 router.get('/notes', (req, res) =>
   fs.readFile('db/db.json','utf-8',(err,data)=> err ? console.log(err) : res.json(JSON.parse(data))
   ));
+
+
+ 
+  // Start the server
+  app.listen(3000, () => {
+    console.log('Server started on port 3000');
+  });
+  
+  
 
 
 
