6a440bb0c86d50fee56feeb7d8c6553dab09611c
anju <anjualfino@gmail.com>
2023-06-05 22:22:37 +0800

    delete updates

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,16 +2,25 @@ const fs = require('fs');
 const router = require('express').Router();
 const express = require('express');
 const app = express();
-const db = require('../db/db.json');
+//const db = require('../db/db.json');
 const { v4: uuidv4 } = require('uuid');
 
 
 // GET Route for homepage
-router.get('/notes', (req, res) =>
-    fs.readFile('db/db.json', 'utf-8', (err, data) => err ? console.log(err) : res.json(JSON.parse(data))
-    ));
+router.get('/notes', (req, res) => {
+    const db = require('../db/db.json');
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
 
 router.post('/notes', (req, res) => {
+    const db = require('../db/db.json');
     const { title, text } = req.body;
     const id = uuidv4();
     const newNote = {
@@ -22,17 +31,15 @@ router.post('/notes', (req, res) => {
     db.push(newNote);
     fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
         if (err) {
-            // Handle the error if writing to the file fails
             console.error(err);
             return res.status(500).json({ error: 'Failed to save the note.' });
         }
-
-        // Send a response indicating the note was successfully saved
         res.json(newNote);
     });
 });
 
 router.delete('/notes/:id', (req, res) => {
+    const db = require('../db/db.json');
     const id = req.params.id;
 
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -50,19 +57,13 @@ router.delete('/notes/:id', (req, res) => {
                 // If the note with the given ID is not found, return a 404 response
                 return res.status(404).json({ error: 'Note not found.' });
             }
-
-            // Remove the note with the given ID from the notes array
             notes.splice(index, 1);
-
-            // Write the updated notes array back to the db.json file
             fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
                 if (err) {
                     // Handle the error if writing to the file fails
                     console.error(err);
                     return res.status(500).json({ error: 'Failed to delete the note.' });
                 }
-
-                // Send a response indicating the note was successfully deleted
                 res.status(204).send();
             });
         } catch (error) {
@@ -71,15 +72,8 @@ router.delete('/notes/:id', (req, res) => {
         }
     });
 });
-
-
-
-
-
 // Start the server
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
-
-
 module.exports = router;
